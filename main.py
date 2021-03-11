d4e723e35dca02331d6a1797aef0d511dd10a15f
Tom Parish <1tomparish@gmail.com>
2021-03-11 19:09:03 +1100

    mainline program to setup program and possibly run test, might not be needed in end product

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Written by Thomas Parish
+Contact: 1tomparish@gmail.com
+
+
+Main sequential program - for now
+Mostly for testing and setting up the window 
+'''
+
+import program
+
+# program starts here
+
+
+# basic testing
+gb = program.Cafe('Georgie Boys')
+
+print(gb.getName())
