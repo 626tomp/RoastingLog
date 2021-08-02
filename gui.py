93542909a6bfa4f6ec8c5d168a686243f213207d
Tom Parish <1tomparish@gmail.com>
2021-08-02 16:21:35 +1000

    push

---
+++ gui.py
@@ -0,0 +1,18 @@
+import tkinter
+
+from tkinter import *
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+# initialize tkinter
+root = Tk()
+app = Window(root)
+
+# set window title
+root.wm_title("Tkinter window")
+
+# show window
+root.mainloop()
