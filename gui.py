ba846723730d3b85b41b376798f51cbd8ad3b618
Tom Parish <1tomparish@gmail.com>
2021-08-19 10:39:16 +1000

    code tidying

--- gui.py
+++ gui.py
@@ -21,15 +21,21 @@ def create_layout(conn):
             sg.Text(item['quantity']),
             sg.Button("+", size=(2, 1), key=f"-increment_{item['id']}-")
         ])
-    test_section = [sg.Button("Refresh", key="-Add_Number-"), sg.Button("Edit", key="-EDIT_ENTRY-")]
 
-    
     layout.append([
             [sg.Listbox(values=[], enable_events=True, size=(40, 20), key="-BLEND LIST-"),
-            sg.Button("Refresh", key="-Add_Number-"), sg.Button("Edit", key="-EDIT_ENTRY-")]
+            sg.Button("Refresh", key="-REFRESH-"), sg.Button("Edit", key="-EDIT_ENTRY-")]
         ])
 
     return layout
+
+def get_formatted_blends(conn):
+    blends = db.query_blend(conn)
+    formatted_blends = []
+    for item in blends:
+        formatted_blends.append(f"{item['id']}: {item['name']} = {item['quantity']}")
+
+    return formatted_blends
 
 if __name__ == "__main__":
     conn = db.connect_database(DATABASE_PATH)
@@ -38,32 +44,37 @@ if __name__ == "__main__":
 
     while True:             
         event, values = window.read()
-        blends = db.query_blend(conn)
-        formatted_blends = []
-        for item in blends:
-            formatted_blends.append(f"{item['id']}: {item['name']} = {item['quantity']}")
-
-
+        
         if event in (sg.WIN_CLOSED, 'Cancel'):
             break
         
+        # increment
         x = re.findall("-increment_\d*-", event)
         if len(x) != 0:
-            x = int("".join(re.findall("\d", x[0])))
+            blends = db.query_blend(conn)
+            x = int("".join(re.findall("\d", x[0]))) # regex to pull off id number
             db.update_blend_quantity(conn, x, blends[x-1]['quantity'] + 5)
 
+        # decrement
         x = re.findall("-decrement_\d*-", event)
         if len(x) != 0:
-            x = int("".join(re.findall("\d", x[0])))
+            blends = db.query_blend(conn)
+            x = int("".join(re.findall("\d", x[0]))) # regex to pull off id number
             # want to find a better way to get the id
             db.update_blend_quantity(conn, x, blends[x-1]['quantity'] - 5)
 
-        if event == "-Add_Number-":
+        # Refresh list
+        if event == "-REFRESH-":
+            formatted_blends = get_formatted_blends(conn)
             window["-BLEND LIST-"].update(formatted_blends)
 
+        # Edit
         if event == "-EDIT_ENTRY-":
             # MAKE A POP UP HERE TO EDIT THE CURRENT ENTRY
-            print("EDITING ", values["-BLEND LIST-"][0])
+            try:
+                print("EDITING ", values["-BLEND LIST-"][0])
+            except:
+                print("You need to select an item")
 
         window.refresh()
 
