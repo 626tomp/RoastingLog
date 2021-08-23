1d9a7c11814d7c66d1457f92effd0f49b668d1c8
Tom Parish <1tomparish@gmail.com>
2021-08-23 11:15:56 +1000

    popup

--- gui.py
+++ gui.py
@@ -18,7 +18,7 @@ def create_layout(conn):
         layout.append([
             sg.Text(item['name']),
             sg.Button("-", size=(2, 1), key=f"-decrement_{item['id']}-"),
-            sg.Text(item['quantity']),
+            sg.Input(item['quantity'], key=f"-QUANTITY_{item['id']}-"),
             sg.Button("+", size=(2, 1), key=f"-increment_{item['id']}-")
         ])
 
@@ -36,6 +36,29 @@ def get_formatted_blends(conn):
         formatted_blends.append(f"{item['id']}: {item['name']} = {item['quantity']}")
 
     return formatted_blends
+
+def popup_test(conn, oldValues):
+    # get (or maybe get passed possible blends)
+    # have drop down for 
+    layout = [  [sg.Text('Update')],
+            [sg.Text("Blend"), sg.OptionMenu(["Seasonal", "Hipster"],key="SELECT-BLEND")], 
+            [sg.OK(), sg.Cancel()]] 
+            # defaultValue=oldValues['name'] ,
+    window = sg.Window("Roasting Log", layout)
+    while True:             
+        event, values = window.read()
+        
+        if event in (sg.WIN_CLOSED, 'Cancel'):
+            break
+
+        if event == "OK":
+            print(oldValues)
+            blend = values['SELECT-BLEND']
+            print(blend)
+            break
+
+        window.refresh()
+    window.close()
 
 if __name__ == "__main__":
     conn = db.connect_database(DATABASE_PATH)
@@ -62,6 +85,8 @@ if __name__ == "__main__":
             x = int("".join(re.findall("\d", x[0]))) # regex to pull off id number
             # want to find a better way to get the id
             db.update_blend_quantity(conn, x, blends[x-1]['quantity'] - 5)
+            print(f"-QUANTITY_{x}-  = {blends[x-1]['quantity'] - 5}")
+            values[f"-QUANTITY_{x}-"] = blends[x-1]['quantity'] - 5
 
         # Refresh list
         if event == "-REFRESH-":
@@ -73,6 +98,10 @@ if __name__ == "__main__":
             # MAKE A POP UP HERE TO EDIT THE CURRENT ENTRY
             try:
                 print("EDITING ", values["-BLEND LIST-"][0])
+                try:
+                    popup_test(conn, values["-BLEND LIST-"])
+                except:
+                    print("Error with popup")
             except:
                 print("You need to select an item")
 
